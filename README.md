c42aa9ad3e41b5d9590f8a17e4c18cce0002149b
Tanner Hicks <tannerhicks@icloud.com>
2024-07-26 12:14:44 -0400

    final changes

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# challenge-nine
+# challenge 9
+  
+  ![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)
+  
+  ## Description
+  proffesional readme generator
+  
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  
+  ## Installation
+  npm install inquirer in termianl
+  
+  ## Usage
+  creates professional readme
+  
+  ## License
+    
+  This project is licensed under the MIT License. For more details, see [https://opensource.org/licenses/MIT].
+  
+  ## Contributing
+  Tanner Hicks
+  
+  ## Tests
+  type 'node index.js' in termianl
+  
+  ## Questions
+  If you have any questions about the project, please reach out to me on GitHub at [TannerHicks02](https://github.com/TannerHicks02) or email me at johndoe@gmail.com.
+  
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,69 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
-}
-
-module.exports = generateMarkdown;
+function renderLicenseBadge(license) {
+    switch (license) {
+      case 'MIT':
+        return '![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)';
+      case 'Apache 2.0':
+        return '![License: Apache 2.0](https://img.shields.io/badge/License-Apache%202.0-blue.svg)';
+      case 'GPL 3.0':
+        return '![License: GPL 3.0](https://img.shields.io/badge/License-GPL%203.0-blue.svg)';
+      default:
+        return '';
+    }
+  }
+  
+  function renderLicenseLink(license) {
+    switch (license) {
+      case 'MIT':
+        return 'https://opensource.org/licenses/MIT';
+      case 'Apache 2.0':
+        return 'https://opensource.org/licenses/Apache-2.0';
+      case 'GPL 3.0':
+        return 'https://opensource.org/licenses/GPL-3.0';
+      default:
+        return '';
+    }
+  }
+  
+  function renderLicenseSection(license) {
+    if (license === 'None') return '';
+    return `## License
+    
+  This project is licensed under the ${license} License. For more details, see [${renderLicenseLink(license)}].`;
+  }
+  
+  function generateMarkdown(data) {
+    return `# ${data.title}
+  
+  ${renderLicenseBadge(data.license)}
+  
+  ## Description
+  ${data.description}
+  
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  
+  ## Installation
+  ${data.installation}
+  
+  ## Usage
+  ${data.usage}
+  
+  ${renderLicenseSection(data.license)}
+  
+  ## Contributing
+  ${data.contributing}
+  
+  ## Tests
+  ${data.tests}
+  
+  ## Questions
+  If you have any questions about the project, please reach out to me on GitHub at [${data.github}](https://github.com/${data.github}) or email me at ${data.email}.
+  `;
+  }
+  
+  module.exports = generateMarkdown;
